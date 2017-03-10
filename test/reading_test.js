3667b3de685c41cfef4137dee182e650e13cc3b1
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-03-11 02:58:22 +0400

    Added reading users test

---
+++ test/reading_test.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+
+describe('Reading records', () => {
+
+	let joe;
+
+		beforeEach((done) => {
+			joe = new User ({name:'Joe'});
+				joe.save()
+		.then(()=> done());
+		});
+
+		it('finds all users with a name of Joe', (done) => {
+
+		User.find({name: 'Joe'})
+		.then ((users) => {
+			console.log(users);
+
+done();
+		});
+
+		
+
+	});
+
+
+
+
+		});
