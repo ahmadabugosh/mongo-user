3b684afd771624b06ce078845b1e484a8f2beee9
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-03-16 13:36:31 +0400

    Finished update tests

--- test/update_test.js
+++ test/update_test.js
@@ -13,16 +13,62 @@ describe('Updating records', () => {
 		.then(()=> done());
 		});
 
-	it('instance type using set n save', (done) => {
+		function assertName(operation,done) {
 
-		joe.set('name', 'Alex');
-		joe.save()
-		.then(() => User.find({}))
+			operation
+			.then(() => User.find({}))
 		.then((users)=> {
 		 assert(users.length===1);
 		 assert(users[0].name==='Alex');
 		 done();
-		 });
+		});
+	}
+
+	it('instance type using set n save', (done) => {
+
+		joe.set('name', 'Alex');
+		assertName(joe.save(),done);
+
+	
+
+	});
+
+
+	it('A model instance can update', (done) => {
+
+
+		assertName(joe.update({'name': 'Alex'}),done);
+	
+
+	});
+
+
+
+	it('A model class can update', (done) => {
+
+assertName(
+		User.update({'name':'Joe'}, {'name':'Alex'}), done);
+	
+
+	});
+
+
+	it('A model class can update one record', (done) => {
+
+assertName(
+		User.findOneAndUpdate({'name':'Joe'}, {'name':'Alex'}), done);
+	
+
+	});
+
+
+
+
+	it('A model class can find a record by id and update it', (done) => {
+
+
+		assertName(
+		User.findByIdAndUpdate(joe._id, {'name':'Alex'}), done);
 	
 
 	});
