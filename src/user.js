66b6d621fc992292fcf074b23ea63b5a406fb0ee
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-03-18 01:01:46 +0400

    Finished validation tests

--- src/user.js
+++ src/user.js
@@ -6,6 +6,11 @@ const UserSchema = new Schema ({
 
 	name: {
 	type:	String,
+	validate: {
+
+		validator: (name) => name.length>2,
+		message: 'Name must be longer than 2 characters.'
+	},
 	required: [true, 'Name is required.']
 	}, 
 	postCount: Number
--- test/validation_test.js
+++ test/validation_test.js
@@ -17,6 +17,30 @@ describe('Validating records', () => {
 
 	});
 
+		it('requires a username longer than 2 characters', () => {
+
+		const user = new User({name: 'Al'});
+		const validationResult= user.validateSync();
+		const {message} = validationResult.errors.name;
+		assert(message==='Name must be longer than 2 characters.');
+	
+
+	});
+
+		it('disallows invalid records from being saved', (done) => {
+
+		const user = new User({name: 'Al'});
+		user.save()
+		.catch((validationResult)=> {
+		const {message} = validationResult.errors.name;
+		assert(message==='Name must be longer than 2 characters.');
+		done();
+
+		});
+	
+
+	});
+
 
 
 	
