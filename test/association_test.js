db4b4c7fe25af77a9395fd437c48caa0f0dd9b32
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-03-24 15:17:12 +0400

    Finished association test

--- test/association_test.js
+++ test/association_test.js
@@ -25,12 +25,40 @@ describe('Associations', () => {
 		.then(()=> done())
 		});
 
-	it.only('Saves a relation between a user and a blogpost', (done) => {
+	it ('Saves a relation between a user and a blogpost', (done) => {
 
 		User.findOne({name:'Joe'})
+		.populate('blogPosts')
 		.then((user)=> {
 
-		console.log(user);
+		assert(user.blogPosts[0].title==='JS is Great');
+			done();
+		});
+
+	});
+
+	it.only ('Saves a full relational graph', (done) => {
+
+		User.findOne({name:'Joe'})
+		.populate({
+			path:'blogPosts',
+			populate:{
+				path:'comments',
+				model:'comment',
+				populate: {
+					path:'user',
+					model:'user'
+				}
+			}
+
+	})
+		.then((user)=> {
+
+		assert(user.name==='Joe');
+		assert(user.blogPosts[0].title==='JS is Great');
+		assert(user.blogPosts[0].comments[0].content==='Congrats on your post!');
+		assert(user.blogPosts[0].comments[0].user.name==='Joe');
+
 			done();
 		});
 
